cb5dcd372eb02797b118cced5e5080d1ae7b4600
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-13 03:26:38 -0600

    Added: login service provider

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LogInComponent } from './log-in/log-in.component';
-
+import { LogInService } from './log-in/log-in.service';
 
 @NgModule({
-  declarations: [ LogInComponent],
+  declarations: [ 
+    LogInComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     AuthRoutingModule
+  ],
+  providers: [
+    LogInService
   ]
 })
 export class AuthModule { }
