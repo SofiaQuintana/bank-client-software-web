e48b63583527dbfd1bd947fb4d460c2ad5ab4340
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-19 00:39:36 -0600

    Added: http request to get client active services

---
+++ src/app/client/dashboard.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { Service } from './service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+   //link to remote server
+   private url = 'https://analisis-bank-server.herokuapp.com/services';
+   private token : string;
+
+   //Header declaration (inmutable)
+   httpOptions = {
+     headers : new HttpHeaders({
+       'Content-Type': 'application/json',
+       'Access-Control-Allow-Origin': '*', 
+       'token': 'my-auth-token'
+     })
+   };
+
+  constructor(private cookieService : CookieService, private http : HttpClient) { 
+    //Se obtiene el token almacenado
+    this.token = cookieService.get('TOKEN');
+    //Se realiza una copia que contiene los cambios incluidos en el token de autorizacion
+    this.httpOptions.headers = this.httpOptions.headers.set('token', this.token);
+  }
+
+  /**
+   * this function sends an http get request to remote server,
+   * to get a list of services or any 401 error.
+   * @returns Observable (http response result)
+   */
+  getServices() : Observable<any> {
+    return this.http.get<Service>(this.url + '/active', this.httpOptions);
+  }
+
+}
