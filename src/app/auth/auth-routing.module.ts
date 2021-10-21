3ac029eba143b673dee8e2bdd447560df32d4659
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-21 02:26:47 -0600

    Added: create user route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
 import { LogInComponent } from './log-in/log-in.component';
 
 const authRoutes : Routes = [
-  { path : 'login', component : LogInComponent }
+  { path : 'login', component : LogInComponent },
+  { path : 'create', component : CreateUserComponent }
 ];
 
 
