3d0d329a21cb40b0b6173f66c13b6182c4aa83e1
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-22 20:24:06 -0600

    Updated: onsubmit actions and added functions for alerts

--- src/app/auth/create-user/create-user.component.ts
+++ src/app/auth/create-user/create-user.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NewUser } from '../new-user';
 import { ConfirmPasswordValidator } from '../password.validator';
 import { UserService } from '../user.service';
@@ -11,9 +12,15 @@ import { UserService } from '../user.service';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
+  //form
   createForm : FormGroup;
   //encargado de controlar si el form ha sido 'enviado'
   submitted = false;
+  //Controls if http response status is 200
+  success = false;
+  //Controls if http response is >400
+  error = false;
+  //used to store server response
   serverResponseObject : any;
 
   ngOnInit() {
@@ -35,15 +42,24 @@ export class CreateUserComponent implements OnInit {
     });
   }
 
-  constructor(private formBuilder : FormBuilder, private userService : UserService) {
+  constructor(private formBuilder : FormBuilder, private userService : UserService, private router : Router) {
    }
 
-   get f(): { [key: string]: AbstractControl } {
+  /**
+   * it helps to obtain form components in an easier way
+   */
+  get f(): { [key: string]: AbstractControl } {
     return this.createForm.controls;
   }
 
-
-
+  /**
+   * 1. if form is invalid, returns
+   * 2. if form is valid
+   *  2.1 obtains data submitted in form
+   *  2.2 sends data to service
+   *    2.2.1 on http response sets success to true and deploys a success alert
+   *    2.2.2 on http error sets error to true and deploys an error alert
+   */
   onSubmit() {
     this.submitted = true;
     if(this.createForm.invalid) {
@@ -52,18 +68,37 @@ export class CreateUserComponent implements OnInit {
       let username = this.f.username.value;
       let password = this.f.password.value;
       let cui = this.f.cui.value;
-      const user_type = 1;
+      const user_type = 1;  
       this.userService.sendNewUserData({username, password, user_type, cui} as NewUser).subscribe(
         (response) => {
+          this.success = true;
           response = this.serverResponseObject;
           console.log(this.serverResponseObject);
         },
         (error : HttpErrorResponse) => {
+          this.serverResponseObject = error;
+          this.error = true;
           console.error(error.error.information_message)
         }
       )
     }
-
   }
 
+  /**
+   * 1. sets success to false.
+   * 2. redirects to login page.
+   */
+  redirectToLogin() {
+    this.success = false;
+    this.router.navigate(['/login']);
+  }
+
+  /**
+   * 1. sets submitted attribute to false
+   * 2. resets form values
+   */
+  clearValues() {
+    this.submitted = false;
+    this.createForm.reset();
+  }
 }
