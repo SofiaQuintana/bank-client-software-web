7becca89aa6ba981049d0b55e1e307294e372295
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-24 02:53:46 -0600

    Added: request service with acc req post

---
+++ src/app/client/request.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { Account } from './account';
+import { AccountRequest } from './account-request/account-request';
+
+@Injectable()
+export class RequestService {
+
+  //link to remote server
+  private url = 'https://analisis-bank-server.herokuapp.com/request';
+  private token : string;
+
+  //Header declaration
+  httpOptions = {
+    headers : new HttpHeaders({
+      'Content-Type': 'application/json',
+      token: 'my-auth-token'
+    })
+  };
+
+  constructor(private cookieService : CookieService, private http : HttpClient) { 
+  }
+
+  sendAccountRequest(account : AccountRequest) : Observable<any> {
+    this.updateToken();
+    return this.http.post<AccountRequest>(this.url + '/account', JSON.stringify(account) ,this.httpOptions);
+  }
+
+  private updateToken() {
+    this.token = this.cookieService.get('TOKEN');
+    this.httpOptions.headers = this.httpOptions.headers.set('token', this.token);
+  }
+}
