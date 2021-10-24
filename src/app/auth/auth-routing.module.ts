fdd29efdae3e090ab5c47cd1b632840c794a2863
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-23 22:32:18 -0600

    Added: log out component routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const authRoutes : Routes = [
   { path : 'login', component : LogInComponent },
   { path : 'create', component : CreateUserComponent },
-  { path : 'password_reset', component : ForgotPasswordComponent }
+  { path : 'password_reset', component : ForgotPasswordComponent },
+  { path : 'logout', component : LogoutComponent}
 ];
 
 
