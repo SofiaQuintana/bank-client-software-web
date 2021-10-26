f3572ddc5e7d83c494a7065b9f9b57424036d1ae
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-25 20:03:20 -0600

    Added: credit card request routing

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth.guard';
 import { AccountRequestComponent } from './account-request/account-request.component';
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
+import { CreditCardRequestComponent } from './credit-card-request/credit-card-request.component';
 
 const routes: Routes = [
   { path: '', component: ClientDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'account_request', component: AccountRequestComponent, canActivate: [AuthGuard] }
+  { path: 'account_request', component: AccountRequestComponent, canActivate: [AuthGuard] },
+  { path: 'credit_card_request', component: CreditCardRequestComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
