43ddec495887c0180d66b60c1af57a144804fd16
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-26 02:24:40 -0600

    Added: account statement route

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth.guard';
 import { AccountRequestComponent } from './account-request/account-request.component';
+import { AccountStatusComponent } from './account-status/account-status.component';
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
 import { CreditCardRequestComponent } from './credit-card-request/credit-card-request.component';
 
 const routes: Routes = [
   { path: '', component: ClientDashboardComponent, canActivate: [AuthGuard] },
   { path: 'account_request', component: AccountRequestComponent, canActivate: [AuthGuard] },
-  { path: 'credit_card_request', component: CreditCardRequestComponent, canActivate: [AuthGuard] }
+  { path: 'credit_card_request', component: CreditCardRequestComponent, canActivate: [AuthGuard] },
+  { path: 'account_statement/:id', component: AccountStatusComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
