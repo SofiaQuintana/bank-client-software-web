c1088d54f2b8bd784f13e3bfe7bd5113dd1d7643
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-25 20:04:51 -0600

    Added: credit card http post request

--- src/app/client/request.service.ts
+++ src/app/client/request.service.ts
@@ -4,6 +4,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { Account } from './account';
 import { AccountRequest } from './account-request/account-request';
+import { CreditCardRequest } from './credit-card-request/credit-card-request';
 
 @Injectable()
 export class RequestService {
@@ -26,6 +27,11 @@ export class RequestService {
   sendAccountRequest(account : AccountRequest) : Observable<any> {
     this.updateToken();
     return this.http.post<AccountRequest>(this.url + '/account', JSON.stringify(account) ,this.httpOptions);
+  }
+
+  sendCreditCardRequest(creditCardRequest : CreditCardRequest) : Observable<any> {
+    this.updateToken();
+    return this.http.post<CreditCardRequest>(this.url+'/credit_card',JSON.stringify(creditCardRequest),this.httpOptions);
   }
 
   private updateToken() {
