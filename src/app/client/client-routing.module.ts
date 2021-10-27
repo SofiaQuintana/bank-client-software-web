86cb3f1eca1b1f7b18502b71081d76d3cd4e86a1
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-26 22:25:06 -0600

    Added: loan statement route

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -5,12 +5,14 @@ import { AccountRequestComponent } from './account-request/account-request.compo
 import { AccountStatusComponent } from './account-status/account-status.component';
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
 import { CreditCardRequestComponent } from './credit-card-request/credit-card-request.component';
+import { LoanStatementComponent } from './loan-statement/loan-statement.component';
 
 const routes: Routes = [
   { path: '', component: ClientDashboardComponent, canActivate: [AuthGuard] },
   { path: 'account_request', component: AccountRequestComponent, canActivate: [AuthGuard] },
   { path: 'credit_card_request', component: CreditCardRequestComponent, canActivate: [AuthGuard] },
-  { path: 'account_statement/:id', component: AccountStatusComponent, canActivate: [AuthGuard] }
+  { path: 'account_statement/:id', component: AccountStatusComponent, canActivate: [AuthGuard] },
+  { path: 'loan_statement/:id', component: LoanStatementComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
