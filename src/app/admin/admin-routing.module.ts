1b03eeb0a5f2dfdfb82c82e6fd468e83371f7e67
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-27 05:15:10 -0600

    Added: user list route as a dashboard child

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthGuard]}
+  { path: '', 
+    component: DashboardComponent, 
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        canActivateChild: [AuthGuard],
+        children: [
+          { path: 'all_users', component: UserListComponent}
+        ]
+      }
+    ]
+
+  }
 ];
 
 @NgModule({
