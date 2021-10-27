3337847f70352bd4b7bba46b88057ca5531e0a3b
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-27 16:33:43 -0600

    Added: missing components and bank user list

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,11 +4,15 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BankUserListComponent } from './bank-user-list/bank-user-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    SidebarComponent
+    SidebarComponent,
+    UserListComponent,
+    BankUserListComponent
   ],
   imports: [
     CommonModule,
