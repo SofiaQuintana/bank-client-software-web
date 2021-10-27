1d6a92780cb56f58a8b9e66102219b4831363ae0
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-27 05:16:04 -0600

    Added: user model

---
+++ src/app/admin/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    username : string;
+    user_type_description : string;
+    user_type: number;
+    cui : number;
+    last_update_date : any;
+    access : boolean;
+}
