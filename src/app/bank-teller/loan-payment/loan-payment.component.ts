b410af18b24bb43a46c9fb25069a139df0642175
AlexanderLuther <helmuthalexander-luthermontejo@cunoc.edu.gt>
2021-10-27 14:37:47 -0600

    Bug con el token de sesion

--- src/app/bank-teller/loan-payment/loan-payment.component.ts
+++ src/app/bank-teller/loan-payment/loan-payment.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OperationService } from '../operation.service';
+import { LoanPayment } from './loan-payment';
 
 @Component({
   selector: 'app-loan-payment',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanPaymentComponent implements OnInit {
 
-  constructor() { }
+
+  submitted = false;
+  isError = false;
+  isSuccess = false;
+  requestForm : FormGroup;
+  serverResponseObject : any;
+
+  constructor(private formBuilder : FormBuilder, private operationService : OperationService, private router : Router) { }
 
   ngOnInit(): void {
+    this.requestForm = this.formBuilder.group({
+      loanId: ['', Validators.required],
+      amount: ['', Validators.required]
+    });
+  }
+
+  /**
+   * it helps to obtain form components in an easier way
+   */
+   get f(): { [key: string]: AbstractControl } {
+    return this.requestForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if(this.requestForm.invalid) return;
+    else {
+      let loanId = this.f.loanId.value;
+      let amount = this.f.amount.value;
+      this.operationService.requestLoanPayment(new LoanPayment(loanId, amount)).subscribe(
+        (response) => {
+          this.isSuccess = true;
+        },
+        (error : HttpErrorResponse) => {
+          this.serverResponseObject = error;
+          this.isError = true;
+        }
+      ) 
+    }
+  }
+
+  redirectToDashboard() {
+    this.isError = this.isSuccess = this.submitted = false;
+    this.router.navigate(['./bank_teller']);
+  }
+
+  cleanForm() {
+    this.isError = this.isSuccess = false;
+    this.requestForm.reset();
   }
 
 }
