b9171c7451eb7de9a8945cefa958a0e4d6036e1e
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-28 01:07:09 -0600

    Added: request list route

--- src/app/request-user/request-user-routing.module.ts
+++ src/app/request-user/request-user-routing.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RequestListComponent } from './request-list/request-list.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthGuard] }
+  { path: '', 
+  component: DashboardComponent, 
+  canActivate: [AuthGuard],
+  children: [
+    {
+      path: '',
+      canActivateChild: [AuthGuard],
+      children: [
+        {path: 'request_list/:id', component: RequestListComponent, canActivate: [AuthGuard]}
+      ]
+    }
+  ]
+}
 ];
 
 @NgModule({
