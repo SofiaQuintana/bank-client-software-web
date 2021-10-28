302af21af065cccd94997047d68175409843a3e8
SofiaQuintana <sofiaquintana2@gmail.com>
2021-10-27 18:43:27 -0600

    Added: transaction model

---
+++ src/app/admin/transaction-list/transaction.ts
@@ -0,0 +1,8 @@
+export interface Transaction {
+    no : number;
+    amount : number;
+    responsible_username : string;
+    id_account : number;
+    movement_type : string;
+    date_time : any;
+}
